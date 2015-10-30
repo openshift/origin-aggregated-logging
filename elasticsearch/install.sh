c1820b666c24b433ecce353419365cbac9dccd5e
Luke Meyer <lmeyer@redhat.com>
2015-10-30 13:33:38 -0400

    ES: fix plugin versions

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -14,8 +14,8 @@ export CLUSTER_NAME=placeholder
 
 mkdir -p ${HOME}
 ln -s /usr/share/elasticsearch /usr/share/java/elasticsearch
-/usr/share/elasticsearch/bin/plugin -i com.floragunn/search-guard/0.6
-/usr/share/elasticsearch/bin/plugin -i io.fabric8.elasticsearch/openshift-elasticsearch-plugin/0.5
+/usr/share/elasticsearch/bin/plugin -i com.floragunn/search-guard/0.5
+/usr/share/elasticsearch/bin/plugin -i io.fabric8.elasticsearch/openshift-elasticsearch-plugin/0.6
 /usr/share/elasticsearch/bin/plugin -i io.fabric8/elasticsearch-cloud-kubernetes/1.3.0
 mkdir /elasticsearch
 chmod -R og+w /usr/share/java/elasticsearch ${HOME} /elasticsearch
