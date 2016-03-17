153b254ce5d2003eb9939a806e88176599eb4010
Rich Megginson <rmeggins@redhat.com>
2016-03-17 14:06:31 -0600

    the right versions of elasticsearch-py and curator are in pypi

--- curator/install.sh
+++ curator/install.sh
@@ -9,20 +9,6 @@ yum install -y --setopt=tsflags=nodocs \
 pip install elasticsearch-curator python-crontab
 yum clean all
 
-# HACK HACK HACK - remove when fixed upstream
-yum install -y --setopt=tsflags=nodocs \
-    git
-git clone -b issue-344 https://github.com/richm/elasticsearch-py.git
-pushd elasticsearch-py
-python setup.py install
-popd
-git clone -b issue-520 https://github.com/richm/curator.git
-pushd curator
-python setup.py install
-popd
-yum clean all
-# HACK HACK HACK - remove when fixed upstream
-
 mkdir -p ${HOME}
 mkdir -p ${CURATOR_CONF_LOCATION}
 touch ${CURATOR_CONF_LOCATION}/settings
