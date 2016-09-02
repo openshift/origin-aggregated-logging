2d1839950e5dff1f1a6c5f1b2016fdfa6e4790a3
ewolinetz <ewolinet@redhat.com>
2016-09-02 10:06:23 -0500

    testing all alias removal

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -10,7 +10,8 @@ ln -s /usr/share/elasticsearch /usr/share/java/elasticsearch
 /usr/share/elasticsearch/bin/plugin install -b file://${HOME}/search-guard-2-2.3.3.6.zip
 /usr/share/elasticsearch/bin/plugin install io.fabric8/elasticsearch-cloud-kubernetes/2.3.3
 
-/usr/share/elasticsearch/bin/plugin install io.fabric8.elasticsearch/openshift-elasticsearch-plugin/2.3.3.1
+#/usr/share/elasticsearch/bin/plugin install io.fabric8.elasticsearch/openshift-elasticsearch-plugin/2.3.3.1
+/usr/share/elasticsearch/bin/plugin install file://${HOME}/openshift-elasticsearch-plugin-2.3.3.2.zip
 
 mkdir /elasticsearch
 mkdir -p $ES_CONF
