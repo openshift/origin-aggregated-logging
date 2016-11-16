49b11bf22734ea704262e09b26e1ddb85f737e39
ewolinetz <ewolinet@redhat.com>
2016-11-16 12:21:55 -0600

    Upgrading to ES 2.4.1 and adding settings to allow dots in the field names

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -7,9 +7,6 @@ ln -s /usr/share/elasticsearch /usr/share/java/elasticsearch
 
 /usr/share/elasticsearch/bin/plugin install -b com.floragunn/search-guard-ssl/${SG_SSL_VER}
 /usr/share/elasticsearch/bin/plugin install -b com.floragunn/search-guard-2/${SG_VER}
-
-# We are currently carrying a patch for sg-2; cannot use normal install
-#/usr/share/elasticsearch/bin/plugin install -b file://${HOME}/search-guard-2-2.3.5.6.zip
 /usr/share/elasticsearch/bin/plugin install io.fabric8/elasticsearch-cloud-kubernetes/${ES_CLOUD_K8S_VER}
 /usr/share/elasticsearch/bin/plugin install io.fabric8.elasticsearch/openshift-elasticsearch-plugin/${OSE_ES_VER}
 
