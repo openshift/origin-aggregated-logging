a1f7c4867f027b412a4aa4010b68ad9349332c6b
Jeff Cantrill <jcantril@redhat.com>
2017-02-28 10:56:19 -0500

    bug 1427274.  Allow users to provide override styles (e.g. header customization)

--- kibana/run.sh
+++ kibana/run.sh
@@ -4,4 +4,13 @@ set -euo pipefail
 sed -i "s/es_host/$ES_HOST/" ${KIBANA_HOME}/config/kibana.yml
 sed -i "s/es_port/$ES_PORT/" ${KIBANA_HOME}/config/kibana.yml
 
+if [ -f "/etc/openshift/kibana/styles/overrides.css" ]; then
+  cp -f /etc/openshift/kibana/styles/overrides.css ${KIBANA_HOME}/installedPlugins/origin-kibana/public/styles
+  rm -rf ${KIBANA_HOME}/optimize/bundles/**
+fi
+
+if [ -d "/etc/openshift/kibana/images" ]; then
+  cp -f /etc/openshift/kibana/images/* ${KIBANA_HOME}/installedPlugins/origin-kibana/public/images
+fi
+
 exec ${KIBANA_HOME}/bin/kibana
