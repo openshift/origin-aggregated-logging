73c2ea7ea5abf43f88f5c6916876f89cf4e25d56
Jeff Cantrill <jcantril@redhat.com>
2017-03-16 16:04:40 -0400

    refactor generate_throttle_configs.rb to enhance logging

--- fluentd/generate_throttle_configs.rb
+++ fluentd/generate_throttle_configs.rb
@@ -2,16 +2,35 @@ require 'yaml'
 require 'date'
 require 'logger'
 
+log_level = ENV['LOG_LEVEL'] || 'WARN'
 @log = Logger.new(STDOUT)
-@log.level = Logger::WARN
 
-@Valid_Settings = {"read_lines_limit" => "number"}
+begin
+  @log.level = eval("Logger::#{log_level}")
+rescue
+  @log.level = Logger::WARN
+  @log.warn "#{log_level} is not a valid value. Must be one of: DEBUG, WARN, INFO, ERROR"
+  @log.warn "Setting log level to WARN"
+end
+
+ENV.sort.each do |entry|
+  @log.debug entry
+end if @log.debug?
+
+DEFAULT_OPS_PROJECTS = ['default', 'openshift', 'openshift-infa']
+DEFAULT_FILENAME = "/etc/fluent/configs.d/user/throttle-config.yaml"
+
+VALID_SETTINGS = {"read_lines_limit" => "number"}
 
 def get_file_name(name, isSyslog)
   ## file_name follows pattern: gen-#{name}-YYYYMMDD.conf ##
 
-  file_name = "/etc/fluent/configs.d/dynamic/input-docker-" unless isSyslog
-  file_name = "/etc/fluent/configs.d/dynamic/input-syslog-" if isSyslog
+  file_name = if isSyslog
+    "/etc/fluent/configs.d/dynamic/input-syslog-"
+  else
+    "/etc/fluent/configs.d/dynamic/input-docker-"
+  end
+
   file_name << name
   file_name << "-"
   file_name << Date.today.strftime("%Y%m%d")
@@ -28,17 +47,14 @@ def seed_file(file_name, project, isSyslog)
       path = "/var/log/messages"
       pos_file = "/var/log/node.log.pos"
     else
-      path = get_project_pattern("default")
-      path << ","
-      path << get_project_pattern("openshift")
-      path << ","
-      path << get_project_pattern("openshift-infra")
+      path = DEFAULT_OPS_PROJECTS.map{|p| get_project_pattern(p)}.join(',')
     end
   else
     path = get_project_pattern(project)
   end
 
   File.open(file_name, 'w') { |file|
+    @log.debug "Seeding #{file_name} with path: '#{path}' and pos_file: '#{pos_file}'"
     file.write(<<-CONF)
 <source>
   @type tail
@@ -49,7 +65,6 @@ def seed_file(file_name, project, isSyslog)
   }
 end
 
-
 def write_to_file(project, key, value, isSyslog)
 
   file_name = get_file_name(project, isSyslog)
@@ -58,6 +73,7 @@ def write_to_file(project, key, value, isSyslog)
   seed_file(file_name, project, isSyslog) if !File.exist?(file_name)
 
   File.open(file_name, 'a') { |file|
+    @log.debug "Writing key: '#{key}' value: '#{value}' to file: #{file_name}"
     file.write(<<-CONF)
   #{key} #{value}
     CONF
@@ -67,10 +83,10 @@ end
 
 def close_file(project, isSyslog)
   file_name = get_file_name(project, isSyslog)
-
-  if isSyslog
-    File.open(file_name, 'a') { |file|
-      file.write(<<-CONF)
+  
+  if File.exist?(file_name)
+    content = if isSyslog
+      %(
   tag system.*
   format multiline
   # Begin possible multiline match: "Mmm DD HH:MM:SS "
@@ -81,19 +97,21 @@ def close_file(project, isSyslog)
   read_from_head true
   keep_time_key true
 </source>
-    CONF
-    } if File.exist?(file_name)
-  else
-    File.open(file_name, 'a') { |file|
-      file.write(<<-CONF)
+      )
+    else
+      %(
   time_format %Y-%m-%dT%H:%M:%S.%N%Z
   tag kubernetes.*
   format json
   keep_time_key true
   read_from_head true
 </source>
-    CONF
-    } if File.exist?(file_name)
+      )
+    end
+    File.open(file_name, 'a') { |file|
+      @log.debug "Closing file: #{file_name}"
+      file.write(content)
+    }
   end
 end
 
@@ -101,8 +119,7 @@ def create_default_docker(excluded)
 
   file_name = "/etc/fluent/configs.d/dynamic/input-docker-default-docker.conf"
 
-  File.open(file_name, 'w') { |file|
-    file.write(<<-CONF)
+  content = %(
 <source>
   @type tail
   @label @INGRESS
@@ -115,7 +132,11 @@ def create_default_docker(excluded)
   read_from_head true
   exclude_path #{excluded}
 </source>
-    CONF
+  )
+
+  @log.debug "Writing content to #{file_name}: #{content}" 
+  File.open(file_name, 'w') { |file|
+    file.write(content)
   }
 end
 
@@ -123,29 +144,21 @@ def create_default_syslog()
 
   file_name = "/etc/fluent/configs.d/dynamic/input-syslog-default-syslog.conf"
 
-  if ENV['USE_JOURNAL'] == "true"
-    File.open(file_name, 'w') { |file|
-      file.write(<<-CONF)
+ # workaround for https://github.com/reevoo/fluent-plugin-systemd/issues/19
+ # set read_from_head true
+  content = if ENV['USE_JOURNAL'] == "true"
+    %(
 <source>
   @type systemd
   @label @INGRESS
   path "#{ENV['JOURNAL_SOURCE'] || '/run/log/journal'}"
   pos_file /var/log/journal.pos
   tag journal
-    CONF
-      # workaround for https://github.com/reevoo/fluent-plugin-systemd/issues/19
-      if ENV['JOURNAL_READ_FROM_HEAD'] == "true"
-        file.write(<<-CONF2)
-  read_from_head true
-      CONF2
-      end
-      file.write(<<-CONF3)
+  #{'read_from_head true' if ENV['JOURNAL_READ_FROM_HEAD'] == "true"}
 </source>
-    CONF3
-    }
+    )
   else
-    File.open(file_name, 'w') { |file|
-      file.write(<<-CONF)
+    %(
 <source>
   @type tail
   @label @INGRESS
@@ -161,15 +174,18 @@ def create_default_syslog()
   read_from_head true
   keep_time_key true
 </source>
-    CONF
-    }
+    )
   end
+  @log.debug "Writing content to #{file_name}: #{content}"
+  File.open(file_name, 'w') { |file|
+    file.write(content)
+  }
 end
 
 def validate(key, value)
   # if the key is in valid settings, validate the value by required type
-  if @Valid_Settings.keys.include?(key)
-    case @Valid_Settings[key]
+  if VALID_SETTINGS.keys.include?(key)
+    case VALID_SETTINGS[key]
     when "time"
       valid = value.to_s.match('^\d+[sSmMhH]$') { |m| if m.nil?; false; else m.to_s.match('\d+').to_s.to_i > 0 end }
     when "size"
@@ -201,34 +217,37 @@ def get_project_pattern(name)
   project_pattern << "_*.log"
 end
 
-filename = "/etc/fluent/configs.d/user/throttle-config.yaml"
-
-parsed = ""
-parsed = YAML.load_file(filename) if File.exists?(filename)
+filename = ENV['THROTTLE_CONF_LOCATION'] || DEFAULT_FILENAME
+parsed = if File.exists?(filename)
+             Hash[YAML.load_file(filename).map{|k,v|[k,v]}]
+         else
+            Hash.new
+         end 
 
 excluded = Array.new
 excludeSyslog = false
 
 # We do not yet support throttling logs read from the journal
 unless ENV['USE_JOURNAL'] == "true"
-  parsed.each { |project|
-    name = project[0]
-    options = project[1]
+  @log.info("Evaluating log trottle settings from #{filename}...")
+
+  parsed.each { |name, options|
+    @log.debug("Evaluating throttling for project '#{name}'")
 
     options.each_pair { |k,v|
 
       if validate(k,v)
         write_to_file(name, k, v, false)
         # build the list of paths the exclude
-        excluded.push(get_project_pattern(name)) unless name.eql?(".operations")
-
         if name.eql?(".operations")
-          excluded.push(get_project_pattern("default"))
-          excluded.push(get_project_pattern("openshift"))
-          excluded.push(get_project_pattern("openshift-infra"))
-
+          @log.debug("Found throttling settings for operations. Excluding projects: #{DEFAULT_OPS_PROJECTS}") 
+          DEFAULT_OPS_PROJECTS.each do |p|
+            excluded.push(get_project_pattern(p))
+          end
           excludeSyslog = true
           write_to_file(name, k, v, true)
+        else
+          excluded.push(get_project_pattern(name))
         end
       else
         @log.warn "Invalid key/value pair {\"#{k}\":\"#{v}\"} provided -- ignoring..."
