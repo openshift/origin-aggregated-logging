f4d71e004af5690f46f287f0fff70ff5cbbb4cb6
Jeff Cantrill <jcantril@redhat.com>
2017-03-20 16:16:54 -0400

    bug 1431551. Seed SG config on start instead of in plugin

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -10,8 +10,18 @@ ln -s /usr/share/elasticsearch /usr/share/java/elasticsearch
 /usr/share/elasticsearch/bin/plugin install io.fabric8/elasticsearch-cloud-kubernetes/${ES_CLOUD_K8S_VER}
 /usr/share/elasticsearch/bin/plugin install io.fabric8.elasticsearch/openshift-elasticsearch-plugin/${OSE_ES_VER}
 
+
 mkdir /elasticsearch
 mkdir -p $ES_CONF
 chmod -R og+w $ES_CONF
 chmod -R og+w /usr/share/java/elasticsearch ${HOME} /elasticsearch
 chmod -R o+rx /etc/elasticsearch
+chmod +x /usr/share/elasticsearch/plugins/search-guard-2/tools/sgadmin.sh
+
+PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+cat > ${HOME}/sgconfig/sg_internal_users.yml << CONF
+---
+  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1):
+    hash: $PASSWD
+CONF
+
