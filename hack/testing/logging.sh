12c12f123c564ae4fce9801ee23356a65bff85b7
Steve Kuznetsov <skuznets@redhat.com>
2017-05-05 15:14:01 -0700

    more debugging

--- hack/testing/logging.sh
+++ hack/testing/logging.sh
@@ -102,10 +102,17 @@ function cleanup()
         os::log::info "Test Succeeded"
     fi
 
+    set -o xtrace
+    echo "########## SELINUX DEBUGGING ##########"
     sudo getenforce
-    sudo ausearch -m AVC -m USER_AVC -m SELINUX_ERR 
+    sudo semodule --list-modules=standard
+    sudo ausearch -m AVC -m USER_AVC -m SELINUX_ERR
     sudo journalctl --no-pager --all --lines=all | grep -i AVC
-  
+    sudo semodule --list-modules=full
+    sudo ausearch -m ALL
+    echo "########## SELINUX DEBUGGING ##########"
+    set +o xtrace
+
     os::test::junit::declare_suite_end
     os::test::junit::reconcile_output
     echo
@@ -345,7 +352,7 @@ function reinstall() {
 echo SKIPPING reinstall test for now
 exit 0
 
-TEST_DIVIDER="------------------------------------------" 
+TEST_DIVIDER="------------------------------------------"
 echo $TEST_DIVIDER
 reinstall
 
