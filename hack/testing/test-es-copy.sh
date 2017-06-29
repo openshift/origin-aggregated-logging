6468d02f2f16bb59f7e08a4cc8e185b287cf2965
Noriko Hosoi <nhosoi@redhat.com>
2017-06-29 12:45:21 -0700

    Bug 1477513 - [3.4] Data loss of logs can occur if fluentd pod is terminated/restarted when Elasticsearch is unavailable
    Backporting: Impl fluentd file buffer.
        Supporting 'buffer_type file' only.
        Changing the default buffer chunk limit to 16M.
        Adding 'public_suffix:<3' to Dockerfile
    
    A new environemnt variable:
        FILE_BUFFER_LIMIT - file buffer size limit per output; default to 2GB
    
    Bump FLUENTD_VERSION to 0.12.39.
    
    (cherry picked from commit 6252ba1d1ae2695a51795339478b2f0a876bced3)

--- hack/testing/test-es-copy.sh
+++ hack/testing/test-es-copy.sh
@@ -9,6 +9,10 @@ fi
 set -o nounset
 set -o pipefail
 
+if ! type get_running_pod > /dev/null 2>&1 ; then
+    . ${OS_O_A_L_DIR:-../..}/deployer/scripts/util.sh
+fi
+
 if [[ $# -ne 1 || "$1" = "false" ]]; then
   # assuming not using OPS cluster
   CLUSTER="false"
@@ -186,15 +190,21 @@ write_and_verify_logs() {
     return $rc
 }
 
-restart_fluentd() {
-    # delete daemonset which also stops fluentd
-    oc delete daemonset logging-fluentd
-    # wait for fluentd to stop
+undeploy_fluentd() {
+    fpod=`get_running_pod fluentd`
+
+    # undeploy fluentd
+    oc label node --all logging-infra-fluentd-
+
     wait_for_pod_ACTION stop $fpod
-    # create the daemonset which will also start fluentd
-    oc process logging-fluentd-template | oc create -f -
-    # wait for fluentd to start
-    wait_for_pod_ACTION start fluentd
+}
+
+redeploy_fluentd() {
+  # redeploy fluentd
+  oc label node --all logging-infra-fluentd=true
+
+  # wait for fluentd to start
+  wait_for_pod_ACTION start fluentd
 }
 
 TEST_DIVIDER="------------------------------------------"
@@ -209,19 +219,15 @@ testport=5432
 # cause messages to be written to the system log - verify that OPS contains
 # two copies
 
-fpod=`get_running_pod fluentd`
+undeploy_fluentd
 
-# first, make sure copy is off
 cfg=`mktemp`
-oc get template logging-fluentd-template -o yaml | \
+# first, make sure copy is off
+oc get daemonset logging-fluentd -o yaml | \
     sed '/- name: ES_COPY/,/value:/ s/value: .*$/value: "false"/' | \
     oc replace -f -
-restart_fluentd
-fpod=`get_running_pod fluentd`
 
-# save original template config
-origconfig=`mktemp`
-oc get template logging-fluentd-template -o yaml > $origconfig
+redeploy_fluentd
 
 # run test to make sure fluentd is working normally - no copy
 write_and_verify_logs 1 || {
@@ -229,13 +235,20 @@ write_and_verify_logs 1 || {
     exit 1
 }
 
+undeploy_fluentd
+
+# save original daemonset config
+origconfig=`mktemp`
+oc get daemonset logging-fluentd -o yaml > $origconfig
+
 cleanup() {
     # may have already been cleaned up
     if [ ! -f $origconfig ] ; then return 0 ; fi
+    undeploy_fluentd
     # put back original configuration
     oc replace --force -f $origconfig
     rm -f $origconfig
-    restart_fluentd
+    redeploy_fluentd
 }
 trap "cleanup" INT TERM EXIT
 
@@ -244,10 +257,15 @@ nocopy=`mktemp`
 sed '/_COPY/,/value/d' $origconfig > $nocopy
 # for every ES_ or OPS_ setting, create a copy called ES_COPY_ or OPS_COPY_
 envpatch=`mktemp`
-sed -n '/^        - env:/,/^          image:/ {
-/^          image:/d
-/^        - env:/d
+sed -n '/^ *- env:/,/^ *image:/ {
+/^ *image:/d
+/^ *- env:/d
 /name: K8S_HOST_URL/,/value/d
+/name: .*JOURNAL.*/,/value/d
+/name: .*BUFFER.*/,/value/d
+/name: FLUENTD_.*_LIMIT/,/valueFrom:/d
+/resourceFieldRef:/,/containerName: fluentd-elasticsearch/d
+/divisor:/,/resource: limits./d
 s/ES_/ES_COPY_/
 s/OPS_/OPS_COPY_/
 p
@@ -255,25 +273,26 @@ p
 
 # add the scheme, and turn on verbose
 cat >> $envpatch <<EOF
-          - name: ES_COPY
-            value: "true"
-          - name: ES_COPY_SCHEME
-            value: https
-          - name: OPS_COPY_SCHEME
-            value: https
-          - name: VERBOSE
-            value: "true"
+        - name: ES_COPY
+          value: "true"
+        - name: ES_COPY_SCHEME
+          value: https
+        - name: OPS_COPY_SCHEME
+          value: https
+        - name: VERBOSE
+          value: "true"
 EOF
 
 # add this back to the dc config
+docopy=`mktemp`
 cat $nocopy | \
-    sed '/^        - env:/r '$envpatch | \
+    sed '/^ *- env:/r '$envpatch > $docopy
+
+cat $docopy | \
     oc replace -f -
 
-rm -f $envpatch $nocopy
-
-restart_fluentd
-fpod=`get_running_pod fluentd`
+redeploy_fluentd
+rm -f $envpatch $nocopy $docopy
 
 write_and_verify_logs 2 || {
     oc get events -o yaml > $ARTIFACT_DIR/all-events.yaml 2>&1
@@ -283,9 +302,6 @@ write_and_verify_logs 2 || {
 # put back original configuration
 oc replace --force -f $origconfig
 rm -f $origconfig
-
-restart_fluentd
-fpod=`get_running_pod fluentd`
 
 write_and_verify_logs 1 || {
     oc get events -o yaml > $ARTIFACT_DIR/all-events.yaml 2>&1
