0dc599ecd210125837edb0c5c22f59213f17d5d9
Rich Megginson <rmeggins@redhat.com>
2017-09-12 15:27:22 -0600

    Vendor origin/hack/lib at 3d2f5f8
    Changelog:
    5a4f740 Remove build docker volumes if OS_BUILD_ENV_CLEAN_BUILD_VOLUME is set.
    c3239dd Rename pkg/sdn to pkg/network, for consistency with its API
    b10b42b hack/env: remove tmp volume if not user specified
    4570e27 Split up SDN master/node/proxy/CNI code
    2291628 Treat the missing binary message as an info not warning
    75737aa mktemp is not LCD bash
    2b44ff7 hack/lib: dedup os::util::host_platform and os::build::host_platform
    ce7709e Use oc adm instead of oadm which might not exist in various installations.
    e29078d hack/env: mount a volume for /tmp
    d278ea6 Bootstrap Origin policies in post start hook
    d5ed80b Check for golang 1.8.
    f661ac9 Correctly propogate error code from image builds up
    a8c09da replace build context setup with init containers
    3880a8b update surrounding files to handle the change
    f8038cc remove deads2k from some of the more specific packages
    4f5da06 Bootstrap OWNERS files for the repository

--- hack/lib/build/constants.sh
+++ hack/lib/build/constants.sh
@@ -19,7 +19,7 @@ readonly OS_OUTPUT_PKGDIR="${OS_OUTPUT}/pkgdir"
 readonly OS_GO_PACKAGE=github.com/openshift/origin
 
 readonly OS_SDN_COMPILE_TARGETS_LINUX=(
-  pkg/sdn/plugin/sdn-cni-plugin
+  pkg/network/sdn-cni-plugin
   vendor/github.com/containernetworking/cni/plugins/ipam/host-local
   vendor/github.com/containernetworking/cni/plugins/main/loopback
 )
@@ -53,6 +53,9 @@ readonly OPENSHIFT_BINARY_SYMLINKS=(
   openshift-recycle
   openshift-sti-build
   openshift-docker-build
+  openshift-git-clone
+  openshift-manage-dockerfile
+  openshift-extract-image-content
   origin
   osc
   oadm
