8886fb8530f41306712673b3bf89a6865aadec77
Rich Megginson <rmeggins@redhat.com>
2017-10-04 09:30:47 -0600

    handle OPS cluster case

--- test/eventrouter.sh
+++ test/eventrouter.sh
@@ -7,9 +7,10 @@ source "${OS_O_A_L_DIR}/hack/testing/util.sh"
 os::test::junit::declare_suite_start "test/eventrouter"
 
 function warn_nonformatted() {
-    local index=$1
+    local es_pod=$1
+    local index=$2
     # check if eventrouter and fluentd with correct ViaQ plugin are deployed
-    local non_formatted_event_count=$( curl_es $espod /$index/_count?q=verb:* | get_count_from_json )
+    local non_formatted_event_count=$( curl_es $es_pod /$index/_count?q=verb:* | get_count_from_json )
     if [ "$non_formatted_event_count" != 0 ]; then
         os::log::warning "$non_formatted_event_count events from eventrouter in index $index were not processed by ViaQ fluentd plugin"
     fi
@@ -23,9 +24,11 @@ if [ -z "$evpod" ]; then
     os::log::warning "Eventrouter not deployed"
 else
     espod=$( get_es_pod es )
+    esopspod=$( get_es_pod es-ops )
+    esopspod=${esopspod:-$espod}
 
-    warn_nonformatted '/project.*'
-    warn_nonformatted '/.operations.*/'
+    warn_nonformatted $espod '/project.*'
+    warn_nonformatted $esopspod '/.operations.*/'
 
-    os::cmd::expect_success_and_not_text "curl_es $espod /.operations.*/_count?q=kubernetes.event.verb:* | get_count_from_json" "^0\$"
+    os::cmd::expect_success_and_not_text "curl_es $esopspod /.operations.*/_count?q=kubernetes.event.verb:* | get_count_from_json" "^0\$"
 fi
