f648f95712d9128ad55019242372b843c3bca44c
Anton Sherkhonov <asherkho@redhat.com>
2017-11-26 14:02:28 -0500

    Elasticsearch 5.5.2 added
    Elasticsearch and Kibana version 5.5.2 added.
    Search-guard version 5.5.2-16.
    Prometheus exporter version 5.5.2.0 is installed from the web.
    
    Norms are removed from `ipaddr` field in templates as required by ES 5.x.
    OpenShift Elasticsearch plugin is temporarily removed.
    Search-guard permissions temporarily adjusted to allow Kibana to view everything.
    Search-guard admin uses client certificates, instead of server certificates.
    Disabled Kibana startup optimization.
    Various unit test fixes.

--- hack/testing/entrypoint.sh
+++ hack/testing/entrypoint.sh
@@ -30,7 +30,7 @@ source "${OS_O_A_L_DIR}/hack/testing/util.sh"
 # fluentd to keep up with when it has 100m cpu (default), on a aws m4.xlarge
 # system for now, remove the limits on fluentd to unblock the tests
 oc get -n logging daemonset/logging-fluentd -o yaml > "${ARTIFACT_DIR}/logging-fluentd-orig.yaml"
-if [[ -z "${USE_DEFAULT_FLUENTD_CPU_LIMIT:-}" && -n "$(oc get ds logging-fluentd -o jsonpath={.spec.template.spec.containers[0].resources.limits.cpu})" ]] ; then
+if [[ -z "${USE_DEFAULT_FLUENTD_CPU_LIMIT:-}" && -n "$(oc get ds -n logging logging-fluentd -o jsonpath={.spec.template.spec.containers[0].resources.limits.cpu})" ]] ; then
     oc patch -n logging daemonset/logging-fluentd --type=json --patch '[
           {"op":"remove","path":"/spec/template/spec/containers/0/resources/limits/cpu"}]'
 fi
--- kibana/install.sh
+++ kibana/install.sh
@@ -4,6 +4,10 @@ set -ex
 set -o nounset
 set -o pipefail
 
+mkdir -p ${HOME}
+cd ${HOME}/
+yum -y install https://artifacts.elastic.co/downloads/kibana/kibana-${KIBANA_VER}-x86_64.rpm
+
 chmod -R og+w "${KIBANA_CONF_DIR}"
 chmod -R 755 ${KIBANA_HOME}/installedPlugins/origin-kibana
 
@@ -11,34 +15,24 @@ source "${HOME}/prep-install.${RELEASE_STREAM}"
 
 # Kibana starts up slowly because it tries to optimize and cache bundles
 # so we start it up as part of install and then stop it
-mv "${KIBANA_CONF_DIR}/kibana.yml" "${KIBANA_CONF_DIR}/hidden_kibana.yml"
-touch "${KIBANA_CONF_DIR}/kibana.yml"
+#mv "${KIBANA_CONF_DIR}/kibana.yml" "${KIBANA_CONF_DIR}/hidden_kibana.yml"
+#touch "${KIBANA_CONF_DIR}/kibana.yml"
 
-touch "${KIBANA_HOME}/kibana.out"
-"${NODE_BIN}" "${KIBANA_HOME}/src/cli" > "${KIBANA_HOME}/kibana.out" &
-pid=$!
+#touch "${KIBANA_HOME}/kibana.out"
+#"${NODE_BIN}" "${KIBANA_HOME}/src/cli" > "${KIBANA_HOME}/kibana.out" &
+#pid=$!
 
-maxwait=$((5*60)) #5 min
-slept=0
-until [ ! -z "${pid}" ] && [ ${slept} -gt ${maxwait} ] ; do
-  if [ -n "$(grep 'Optimization of bundles for kibana and statusPage complete' ${KIBANA_HOME}/kibana.out)" ] ; then
-      break
-  fi
-  slept=$((slept+1))
-  sleep 1
-done
+#until [ ! -z "${pid}" ] && [ -n "$(grep 'Optimization of bundles for kibana and statusPage complete' ${KIBANA_HOME}/kibana.out)" ]; do
+#  sleep 1
+#done
 
-if [ ${slept} -ge ${maxwait} ] ; then
-   echo "Timed out trying to optimize bundle. Dumping log and continuing..."
-   cat "${KIBANA_HOME}/kibana.out"
-fi
+#if [ ! -z "${pid}" ] ; then
+#  kill $pid
+#fi
 
-if [ ! -z "${pid}" ] ; then
-  kill $pid
-fi
-
-rm "${KIBANA_HOME}/kibana.out"
-rm "${KIBANA_CONF_DIR}/kibana.yml"
-mv "${KIBANA_CONF_DIR}/hidden_kibana.yml" "${KIBANA_CONF_DIR}/kibana.yml"
+#rm "${KIBANA_HOME}/kibana.out"
+#rm "${KIBANA_CONF_DIR}/kibana.yml"
+#mv "${KIBANA_CONF_DIR}/hidden_kibana.yml" "${KIBANA_CONF_DIR}/kibana.yml"
 chmod -R og+w "${HOME}"
 chmod -R og+rw "${KIBANA_HOME}"
+
