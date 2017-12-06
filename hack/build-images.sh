e8e05c0b5735c2b6adec1c62ecdd9713b5cda845
Jeff Cantrill <jcantril@redhat.com>
2017-12-06 13:51:43 -0500

    fix build-images to handle correct Dockerfile for kibana-proxy

--- hack/build-images.sh
+++ hack/build-images.sh
@@ -5,6 +5,7 @@ set -o nounset
 set -o pipefail
 
 STARTTIME=$(date +%s)
+RELEASE_STREAM=${RELEASE_STREAM:-origin}
 
 prefix="${PREFIX:-docker.io/openshift/origin-}"
 version="${OS_TAG:-latest}" 
@@ -25,10 +26,14 @@ for component in ${!source_for[@]} ; do
   BUILD_STARTTIME=$(date +%s)
   comp_path=$source_root/${source_for[$component]}
   docker_tag=${prefix}${component}:${version}
+  dockerfile=${comp_path}/Dockerfile
+  if [[ "${RELEASE_STREAM}" == "origin" ]] && [[ -f "${comp_path}/Dockerfile.centos7" ]]; then
+    dockerfile=${comp_path}/Dockerfile.centos7
+  fi
   echo
   echo
   echo "--- Building component '$comp_path' with docker tag '$docker_tag' ---"
-  docker build -t $docker_tag       $comp_path
+  docker build -t $docker_tag -f ${dockerfile} $comp_path
   BUILD_ENDTIME=$(date +%s); echo "--- $docker_tag took $(($BUILD_ENDTIME - $BUILD_STARTTIME)) seconds ---"
   echo
   echo
