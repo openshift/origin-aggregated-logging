e29a0df3cdde91a4226518fce9372f820052b173
Noriko Hosoi <nhosoi@redhat.com>
2017-12-22 17:00:39 -0800

    Since Time class is declared in out_syslog.rb, out_syslog_buffered.rb does not have to do it again.

--- fluentd/out_syslog_buffered.rb
+++ fluentd/out_syslog_buffered.rb
@@ -135,19 +135,6 @@ module Fluent
         end
       end
     end
-
-
-  end
-
-  class Time < Time
-    def timezone(timezone = 'UTC')
-      old = ENV['TZ']
-      utc = self.dup.utc
-      ENV['TZ'] = timezone
-      output = utc.localtime
-      ENV['TZ'] = old
-      output
-    end
   end
 end
 
