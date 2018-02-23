92da5a9e5ae71ba0f1df38ddfe04131890695f4f
Lukáš Vlček <lukas.vlcek@gmail.com>
2018-02-23 16:07:31 +0100

    Upgrade to common data model 0.0.13
    - The main reason is getting the `.all` alias in index temlpates
    - Update hack/update-viaq-data-model.sh that can correctly work with the lastest common data release
    
    See #972

--- hack/update-viaq-data-model.sh
+++ hack/update-viaq-data-model.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+# Script downloads elasticsearch templates for specified ES version only
+# and removes the ES version pattern from the file name before storing it.
+#
+# ES version must be supplied as an argument.
+#
+# Example:
+# $ update-viaq-data-model.sh 5.5.2
+
 set -euxo pipefail
+
+if [ $# -eq 0 ]
+  then
+    echo "No supported ES version supplied as an argument. (Eg. 2.4.4 or 5.5.2)"
+fi
+ES_VERSION=$1
+echo "Downloading resources for ES version '${ES_VERSION}'"
 
 RELEASE_URL=${RELEASE_URL:-https://api.github.com/repos/ViaQ/elasticsearch-templates/releases/latest}
 
@@ -16,9 +31,9 @@ while [ "${1:-}" ] ; do
     url="$1"; shift
     case "$name" in
         *.asciidoc) dest="docs/$name" ;;
-        *.template.json) dest="elasticsearch/index_templates/$name" ;;
-        *.index-pattern.json) dest="elasticsearch/index_patterns/$name" ;;
-        *) echo Error: unknown name $name; exit 1 ;;
+        *.${ES_VERSION}.template.json) dest="elasticsearch/index_templates/$name"; dest=${dest//.${ES_VERSION}/} ;;
+        *.${ES_VERSION}.index-pattern.json) dest="elasticsearch/index_patterns/$name"; dest=${dest//.${ES_VERSION}/} ;;
+        *) echo Skipping file $name; continue ;;
     esac
     curl -s -L "$url" > "$dest"
 done
