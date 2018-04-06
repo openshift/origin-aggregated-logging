17db82836e24252766b84bc8d71f7445552f1930
Rich Megginson <rmeggins@redhat.com>
2018-04-06 16:00:27 -0600

    improve mux-client-mode debugging

--- test/mux-client-mode.sh
+++ test/mux-client-mode.sh
@@ -8,9 +8,9 @@ os::util::environment::use_sudo
 
 # only works if there is a mux dc
 if oc get dc/logging-mux > /dev/null 2>&1 ; then
-    os::log::debug "$( oc get dc/logging-mux )"
+    oc get dc/logging-mux 2>&1 | artifact_out
 else
-    os::log::debug "$( oc get dc/logging-mux )"
+    oc get dc/logging-mux 2>&1 | artifact_out
     os::log::info dc/logging-mux is not present - skipping test
     exit 0
 fi
@@ -28,19 +28,19 @@ cleanup() {
     set +e
     # dump the pods before we restart them
     if [ -n "${fpod:-}" ] ; then
-        oc logs $fpod > $ARTIFACT_DIR/$fpod.log 2>&1
+        oc logs $fpod > $ARTIFACT_DIR/mux-client-mode-fluentd-pod.log 2>&1
     fi
     if [ -n "${muxpod:-}" ] ; then
-        oc logs $muxpod > $ARTIFACT_DIR/$muxpod.log 2>&1
+        oc logs $muxpod > $ARTIFACT_DIR/mux-client-mode-mux-pod.log 2>&1
     fi
     if [ -n "${saveds:-}" ] ; then
         if [ -f "${saveds:-}" ]; then
-            os::log::debug "$( oc replace --force -f $saveds )"
+            oc replace --force -f $saveds 2>&1 | artifact_out
             rm -f $saveds
         fi
     fi
     os::cmd::expect_success flush_fluentd_pos_files
-    os::log::debug "$( oc label node --all logging-infra-fluentd=true || : )"
+    oc label node --all logging-infra-fluentd=true 2>&1 | artifact_out
     os::cmd::try_until_text "oc get pods -l component=fluentd" "^logging-fluentd-.* Running "
     # this will call declare_test_end, suite_end, etc.
     os::test::junit::reconcile_output
@@ -52,8 +52,8 @@ reset_fluentd_daemonset() {
   muxcerts=$( oc get daemonset logging-fluentd -o yaml | egrep muxcerts ) || :
 
   if [ "$muxcerts" = "" ]; then
-      os::log::debug "$( oc set volumes daemonset/logging-fluentd --add --overwrite \
-               --name=muxcerts --default-mode=0400 -t secret -m /etc/fluent/muxkeys --secret-name logging-mux 2>&1 )"
+      oc set volumes daemonset/logging-fluentd --add --overwrite \
+               --name=muxcerts --default-mode=0400 -t secret -m /etc/fluent/muxkeys --secret-name logging-mux 2>&1 | artifact_out
   fi
 }
 
@@ -61,24 +61,24 @@ fpod=$( get_running_pod fluentd )
 muxpod=$( get_running_pod mux )
 
 os::log::info configure fluentd to use MUX_CLIENT_MODE=minimal - verify logs get through
-os::log::debug "$( oc label node --all logging-infra-fluentd- )"
+oc label node --all logging-infra-fluentd- 2>&1 | artifact_out
 os::cmd::try_until_text "oc get daemonset logging-fluentd -o jsonpath='{ .status.numberReady }'" "0" $FLUENTD_WAIT_TIME
-os::log::debug "$( oc set env daemonset/logging-fluentd MUX_CLIENT_MODE=minimal )"
+oc set env daemonset/logging-fluentd MUX_CLIENT_MODE=minimal 2>&1 | artifact_out
 reset_fluentd_daemonset
 os::cmd::expect_success flush_fluentd_pos_files
-os::log::debug "$( oc label node --all logging-infra-fluentd=true )"
+oc label node --all logging-infra-fluentd=true 2>&1 | artifact_out
 os::cmd::try_until_text "oc get pods -l component=fluentd" "^logging-fluentd-.* Running "
 fpod=$( get_running_pod fluentd )
 wait_for_fluentd_to_catch_up
 
 # configure fluentd to use MUX_CLIENT_MODE=maximal - verify logs get through
 os::log::info configure fluentd to use MUX_CLIENT_MODE=maximal - verify logs get through
-os::log::debug "$( oc label node --all logging-infra-fluentd- )"
+oc label node --all logging-infra-fluentd- 2>&1 | artifact_out
 os::cmd::try_until_text "oc get daemonset logging-fluentd -o jsonpath='{ .status.numberReady }'" "0" $FLUENTD_WAIT_TIME
-os::log::debug "$( oc set env daemonset/logging-fluentd MUX_CLIENT_MODE=maximal )"
+oc set env daemonset/logging-fluentd MUX_CLIENT_MODE=maximal 2>&1 | artifact_out
 reset_fluentd_daemonset
 os::cmd::expect_success flush_fluentd_pos_files
-os::log::debug "$( oc label node --all logging-infra-fluentd=true )"
+oc label node --all logging-infra-fluentd=true 2>&1 | artifact_out
 os::cmd::try_until_text "oc get pods -l component=fluentd" "^logging-fluentd-.* Running "
 fpod=$( get_running_pod fluentd )
 wait_for_fluentd_to_catch_up
