eb4faae60fea2899fe4bf3e696f8ea15f5d37d37
Eric Wolinetz <ewolinet@redhat.com>
2018-04-24 12:16:56 -0500

    Adding prometheus plugin, sources, and exposing endpoints

---
+++ hack/testing/check-zzz-fluentd-prometheus-scrape.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source "$(dirname "${BASH_SOURCE[0]}" )/../lib/init.sh"
+source "${OS_O_A_L_DIR}/hack/testing/util.sh"
+
+os::test::junit::declare_suite_start "test/check-zzz-fluentd-prometheus-scrape"
+
+cleanup() {
+    local return_code="$?"
+    set +e
+
+    # this will call declare_test_end, suite_end, etc.
+    os::test::junit::reconcile_output
+    exit $return_code
+}
+trap "cleanup" EXIT
+
+LOGGING_NS=${LOGGING_NS:-openshift-logging}
+
+fpod="$(get_running_pod fluentd)"
+fpod_ip="$(oc get pod ${fpod} -o jsonpath='{.status.podIP}')"
+
+os::cmd::expect_success "curl http://${fpod_ip}:24231/metrics"
+curl http://${fpod_ip}:24231/metrics >> $ARTIFACT_DIR/${fpod}-metrics-scrape 2>&1 || :
