0db9f7fb6954e92432bc376dd029dd36a49c1671
Josef Karasek <jkarasek@redhat.com>
2018-05-14 11:05:47 +0200

    Set log level on module loggers instead of on root logger

--- curator/src/util.py
+++ curator/src/util.py
@@ -5,5 +5,6 @@ import logging
 
 def create_logger(name):
     lvl = logging._levelNames.get(os.getenv('CURATOR_SCRIPT_LOG_LEVEL', 'INFO'))
-    logging.basicConfig(level=lvl)
-    return logging.getLogger(name)
+    logger = logging.getLogger(name)
+    logger.setLevel(lvl)
+    return logger
