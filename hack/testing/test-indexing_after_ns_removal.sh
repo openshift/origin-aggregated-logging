f8aa6a64406101edf26c3984187296cf71c26a0b
Nicolas Nosenzo <niconosenzo@gmail.com>
2018-05-25 13:12:30 +0200

    Added https://bugzilla.redhat.com/show_bug.cgi?id=1575662#c5

---
+++ hack/testing/test-indexing_after_ns_removal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source "$(dirname "${BASH_SOURCE[0]}" )/../lib/init.sh"
+
+exec ${OS_O_A_L_DIR}/test/indexing_after_ns_removal.sh
+
--- test/indexing_after_ns_removal.sh
+++ test/indexing_after_ns_removal.sh
@@ -32,6 +32,9 @@ cleanup(){
     # delete all the $NS indices
     curl_es $espod /project.$NS.* -XDELETE || :
 
+    # remove temp files used to store uid data
+    rm -f temp_ns_uid temp_pod_uid 
+
     # this will call declare_test_end, suite_end, etc.
     os::test::junit::reconcile_output
 
@@ -48,14 +51,22 @@ espod=$( get_es_pod es )
 for i in $( seq 1 20 ); 
 do
     oc create namespace $NS &>/dev/null
+
     # wait till the project get created
     while ! oc get namespace $NS &>/dev/null; do sleep 1; done
 
-    os::log::info Creating project  $(oc get namespace $NS -o jsonpath={.metadata.uid})
+    os::log::info Creating project $NS with UID $(oc get namespace $NS -o jsonpath={.metadata.uid})
     create_pod $i
     # wait till the pod get created
     while ! oc get pod bar -n $NS &>/dev/null; do sleep 1; done
     sleep 5s
+    os::log::info Creating pod bar with UID $(oc get pod -n $NS -o jsonpath={.metadata.uid})
+
+    # store UID for both NS and pod
+    oc get namespace $NS -o jsonpath={.metadata.uid}  >> temp_ns_uid && echo "" >> temp_ns_uid
+    oc get pod bar -n $NS -o jsonpath={.metadata.uid}  >> temp_pod_uid && echo "" >> temp_pod_uid
+
+    # delete NS
     oc delete namespace $NS &>/dev/null
     while oc get namespace $NS &>/dev/null; do sleep 1; done
 done
@@ -64,9 +75,13 @@ done
 curl_es $es_pod /_cat/indices 2>&1 | artifact_out
 
 # Test indexing, if the amount of indices created is other than 20, exit with 1:
-amount_of_idx=$(oc rsh -c elasticsearch $espod es_util  --query=_cat/indices?h=index,docs.count | grep pelle | wc -l)
-if [ $amount_of_idx -ne 20 ]; then  exit 1; fi
+amount_of_idx=$(curl_es $es_pod /_cat/indices?h=index,docs.count | grep pelle | wc -l)
 
+if [ $amount_of_idx -ne 20 ]
+then  
+    os::log::error Amount of indices created is $amount_of_idx which is less than the amount of projects created
+    exit 1
+fi
 
 ## example of wrong indexing:
 # oc rsh -c elasticsearch $espod es_util  --query=_cat/indices?h=index,docs.count | grep pelle 
@@ -74,3 +89,24 @@ if [ $amount_of_idx -ne 20 ]; then  exit 1; fi
 # project.pelle-foo.53a8d3e3-42f7-11e8-a037-fa163e2c1fb0.2018.04.18                 3 
 # project.pelle-foo.5b71ac2c-42f7-11e8-a037-fa163e2c1fb0.2018.04.18                13 
 # project.pelle-foo.6b04cc03-42f7-11e8-a037-fa163e2c1fb0.2018.04.18                 4
+
+# verify that the kubernetes metadata in the record matches the object data
+# stored in temp_ns_uid and temp_pod_uid files
+# Note: If we reach this point, means there are 20 indices with one document
+# each, so no need to iterate through documents within an index but only over
+# the indices.
+
+for index in $(curl_es $es_pod /_cat/indices?h=index | grep pelle-foo)
+do
+    namespace_uid=$(curl_es $es_pod /"${index}"/_search?q=kubernetes.namespace_id:* | jq . | grep -i namespace_id | awk -F'"' '{ print $4 }')
+    grep -m 1 "${namespace_uid}" temp_ns_uid &>/dev/null
+    [ "$?" != "0" ] && os::log::error ${namespace_uid} does not match any UID collected from the actual namespace list during namespace creation
+
+    pod_uid=$(curl_es $es_pod /"${index}"/_search?q=kubernetes.namespace_id:* | jq . | grep -i pod_id | awk -F'"' '{ print $4 }')
+    grep -m 1 "${pod_uid}" temp_ns_uid &>/dev/null
+    [ "$?" != "0" ] && os::log::error ${pod_id} does not match any UID collected from the actual pod list during pod creation
+done
+
+
+# .orphaned index  is not created by default, so if it exists, then it contains records
+[[ $(curl_es $es_pod /_cat/indices?h=index,docs.count | grep .orphaned) ]] && os::log::error  There are records in the .orphaned index.
