12f56bb6b1c4d3d76111d65e0cd6c7921545ced9
Jeff Cantrill <jcantril@redhat.com>
2018-06-22 14:26:02 -0400

    fix downstream to depend on rpm

--- kibana/install.sh
+++ kibana/install.sh
@@ -6,10 +6,10 @@ set -o pipefail
 
 source "${HOME}/prep-install.${RELEASE_STREAM}"
 
-ORIGIN_KIBANA_PLUGIN=$(ls -t1 ${HOME}/origin-kibana-v$KIBANA_VER-*.zip | head -1)
+ORIGIN_KIBANA_PLUGIN=$(ls -t1 ${HOME}/origin-kibana-v${KIBANA_VER}*.zip | head -1)
 ${KIBANA_HOME}/bin/kibana-plugin install file://${ORIGIN_KIBANA_PLUGIN}
 
 chmod -R og+w "${HOME}"
 chmod -R og+rw "${KIBANA_HOME}"
-chmod -R og+rw /var/lib/kibana
+mkdir -m og+w -p /var/lib/kibana
 chmod -R og+w "${KIBANA_CONF_DIR}"
