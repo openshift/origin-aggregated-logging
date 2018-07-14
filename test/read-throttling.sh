d9f579b0950bed398ae2a454131dacf1e2f356c1
Rich Megginson <rmeggins@redhat.com>
2018-07-13 19:30:17 -0600

    debug remote-syslog flakes

--- test/read-throttling.sh
+++ test/read-throttling.sh
@@ -26,8 +26,12 @@ savecm=$( mktemp )
 oc get configmap logging-fluentd -o yaml > $savecm
 
 stop_fluentd() {
+  artifact_log at this point there should be 1 fluentd running in Running state
+  oc get pods 2>&1 | artifact_out
   os::log::debug "$( oc label node --all logging-infra-fluentd- 2>&1 || : )"
   os::cmd::try_until_text "oc get daemonset logging-fluentd -o jsonpath='{ .status.numberReady }'" "0" $FLUENTD_WAIT_TIME
+  artifact_log at this point there should be no fluentd running - number ready is 0
+  oc get pods 2>&1 | artifact_out
 }
 
 start_fluentd() {
