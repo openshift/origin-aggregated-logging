787355d319d8897a3972e7ef1d89d0f4d56048cd
Rich Megginson <rmeggins@redhat.com>
2018-10-09 14:48:26 -0600

    rollout test fixes
    (cherry picked from commit 1b34c48e5d05f220cc40b88604dc405aea8bb441)
    (cherry picked from commit 4415ee2de10d745f93da05ade995c03859051ba0)

--- test/cluster/rollout.sh
+++ test/cluster/rollout.sh
@@ -14,10 +14,31 @@
 #                  }: $IFS-delimited lists of
 #    OpenShift ojects that are expected to exist
 source "$(dirname "${BASH_SOURCE[0]}" )/../../hack/lib/init.sh"
-trap os::test::junit::reconcile_output EXIT
 
 LOGGING_NS=${LOGGING_NS:-openshift-logging}
 FLUENTD_WAIT_TIME=$(( 2 * minute ))
+
+cleanup() {
+  local return_code="$?"
+  set +e
+  for r in dc ds cronjob ; do
+    oc describe -n ${LOGGING_NS} $r > $ARTIFACT_DIR/$r.describe 2>&1
+  done
+
+  for p in $(oc get pods -n ${LOGGING_NS} -l component=es -o jsonpath={.items[*].metadata.name}) ; do
+    oc logs -c elasticsearch -n ${LOGGING_NS} $p || :  > $ARTIFACT_DIR/$p.rollout.stdout.logs 2>&1
+    oc logs -c proxy -n ${LOGGING_NS} $p > $ARTIFACT_DIR/$p.proxy.stdout.logs 2>&1 || :
+    oc exec -c elasticsearch -n ${LOGGING_NS} $p -- logs || :  > $ARTIFACT_DIR/$p.rollout.file.logs 2>&1
+  done
+  for p in $(oc get pods -n ${LOGGING_NS} -l component!=es -o jsonpath={.items[*].metadata.name}) ; do
+    oc logs -n ${LOGGING_NS} $p || :  > $ARTIFACT_DIR/$p.stdout.logs 2>&1
+  done
+  oc get -n ${LOGGING_NS} --all 2>&1 | artifact_out
+  
+  os::test::junit::reconcile_output
+  exit $return_code
+}
+trap "cleanup" EXIT
 
 os::test::junit::declare_suite_start "test/cluster/rollout"
 
@@ -26,7 +47,13 @@ os::cmd::expect_success "oc project ${LOGGING_NS}"
 os::log::info "Checking for DeploymentConfigurations..."
 for deploymentconfig in ${OAL_EXPECTED_DEPLOYMENTCONFIGS}; do
 	os::cmd::expect_success "oc get deploymentconfig ${deploymentconfig}"
-	os::cmd::expect_success "oc rollout status deploymentconfig/${deploymentconfig}"
+# this is to get around the current kubelet flake where we need to re-rollout to get the dc running
+#  for sanity sake and to not get stuck in the case of real issues, we will only do this once per dc
+	if ! oc rollout status deploymentconfig/${deploymentconfig} ; then
+		os::cmd::expect_success "oc rollout cancel deploymentconfig/${deploymentconfig}"
+		os::cmd::expect_success "oc rollout latest deploymentconfig/${deploymentconfig}"
+		os::cmd::expect_success "oc rollout status deploymentconfig/${deploymentconfig}"
+	fi
 done
 
 os::log::info "Checking for Routes..."
@@ -50,5 +77,3 @@ for daemonset in ${OAL_EXPECTED_DAEMONSETS}; do
 	desired_number="$( oc get daemonset "${daemonset}" -o jsonpath='{ .status.desiredNumberScheduled }' )"
 	os::cmd::try_until_text "oc get daemonset ${daemonset} -o jsonpath='{ .status.numberReady }'" "${desired_number}" $FLUENTD_WAIT_TIME
 done
-
-os::test::junit::declare_suite_end
