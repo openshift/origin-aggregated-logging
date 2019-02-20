8e9a43149052538df665608c9e1f8ccea8df9424
Rich Megginson <rmeggins@redhat.com>
2019-02-19 18:42:16 -0700

    add support for new fluentd viaq filter undefined fields
    add support for new fluentd viaq filter undefined fields
    keeps the default values the same as what we have now, namely
    - do not automatically convert undefined fields to string
    - do not convert `.` to `_` in undefined field names
    - no maximum number of fields
    
    We don't want to break users who might be doing just fine
    with the defaults, but we want to give users the ability
    to turn on these knobs.
    
    Cause: Using MERGE_JSON_LOG=true can create fields in the
    record which will cause schema and syntax violations in
    Elasticsearch.  It can also create too many fields for
    Elasticsearch to handle without severe performance problems.
    
    Consequence: Fluentd reports error 400 sending records to
    Elasticsearch.  Elasticsearch performance degrades.
    
    Fix: Allow users who experience these problems to tune their
    Fluentd to accomodate their log record fields.
    
    Result: Logs are ingested into Fluentd with no errors.
    Elasticsearch performance does not degrade.
    
    (cherry picked from commit f59ebef7e182d479d56cd1ea7c4958e2abeb8dc4)

--- test/json-parsing.sh
+++ test/json-parsing.sh
@@ -14,6 +14,30 @@ if [ -n "${DEBUG:-}" ] ; then
     set -x
 fi
 
+FLUENTD_WAIT_TIME=${FLUENTD_WAIT_TIME:-$(( 2 * minute ))}
+
+stop_fluentd() {
+  artifact_log at this point there should be 1 fluentd running in Running state
+  oc get pods 2>&1 | artifact_out
+  local fpod=$( get_running_pod fluentd )
+  oc label node --all logging-infra-fluentd- 2>&1 | artifact_out
+  os::cmd::try_until_text "oc get daemonset logging-fluentd -o jsonpath='{ .status.numberReady }'" "0" $FLUENTD_WAIT_TIME
+  artifact_log at this point there should be no fluentd running - number ready is 0
+  oc get pods 2>&1 | artifact_out
+  # for some reason, in this test, after .status.numberReady is 0, the fluentd pod hangs around
+  # in the Terminating state for many seconds, which seems to cause problems with subsequent tests
+  # so, we have to wait for the pod to completely disappear - we cannot rely on .status.numberReady == 0
+  if [ -n "${fpod:-}" ] ; then
+    os::cmd::try_until_failure "oc get pod $fpod > /dev/null 2>&1" $FLUENTD_WAIT_TIME
+  fi
+}
+
+start_fluentd() {
+  sudo rm -f /var/log/fluentd/fluentd.log
+  oc label node --all logging-infra-fluentd=true 2>&1 | artifact_out
+  os::cmd::try_until_text "oc get pods -l component=fluentd" "^logging-fluentd-.* Running " $FLUENTD_WAIT_TIME
+}
+
 cleanup() {
     local return_code="$?"
     set +e
@@ -27,6 +51,19 @@ cleanup() {
     if [ -n "${fpod:-}" ] ; then
         get_fluentd_pod_log > $ARTIFACT_DIR/json-parsing-fluentd-pod.log
     fi
+    stop_fluentd
+    if [ "${orig_MERGE_JSON_LOG:-}" = unset ] ; then
+        orig_MERGE_JSON_LOG="MERGE_JSON_LOG-"
+    fi
+    if [ "${orig_CDM_UNDEFINED_TO_STRING:-}" = unset ] ; then
+        orig_CDM_UNDEFINED_TO_STRING="CDM_UNDEFINED_TO_STRING-"
+    fi
+    if [ -n "${orig_MERGE_JSON_LOG:-}" -o -n "${orig_CDM_UNDEFINED_TO_STRING:-}" ] ; then
+        stop_fluentd
+        oc set env daemonset/logging-fluentd ${orig_MERGE_JSON_LOG:-} ${orig_CDM_UNDEFINED_TO_STRING:-}
+        start_fluentd
+    fi
+
     # this will call declare_test_end, suite_end, etc.
     os::test::junit::reconcile_output
     exit $return_code
@@ -34,6 +71,19 @@ cleanup() {
 trap "cleanup" EXIT
 
 os::log::info Starting json-parsing test at $( date )
+
+# enable merge json log
+orig_MERGE_JSON_LOG=$( oc set env daemonset/logging-fluentd --list | grep \^MERGE_JSON_LOG= ) || :
+if [ -z "$orig_MERGE_JSON_LOG" ] ; then
+    orig_MERGE_JSON_LOG=unset
+fi
+orig_CDM_UNDEFINED_TO_STRING=$( oc set env daemonset/logging-fluentd --list | grep \^CDM_UNDEFINED_TO_STRING= ) || :
+if [ -z "$orig_CDM_UNDEFINED_TO_STRING" ] ; then
+    orig_CDM_UNDEFINED_TO_STRING=unset
+fi
+stop_fluentd
+oc set env daemonset/logging-fluentd MERGE_JSON_LOG=true CDM_UNDEFINED_TO_STRING=false
+start_fluentd
 
 # generate a log message in the Kibana logs - Kibana log messages are in JSON format:
 # {"type":"response","@timestamp":"2017-04-07T02:03:37Z","tags":[],"pid":1,"method":"get","statusCode":404,"req":{"url":"/ca30cead-d470-4db8-a2a2-bb71439987e2","method":"get","headers":{"user-agent":"curl/7.29.0","host":"localhost:5601","accept":"*/*"},"remoteAddress":"127.0.0.1","userAgent":"127.0.0.1"},"res":{"statusCode":404,"responseTime":3,"contentLength":9},"message":"GET /ca30cead-d470-4db8-a2a2-bb71439987e2 404 3ms - 9.0B"}
