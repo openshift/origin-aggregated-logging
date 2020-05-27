1140fb87830404c6f6af385dec946d957d99f161
Eric Wolinetz <ewolinet@redhat.com>
2020-05-27 17:25:04 -0500

    Adding Makefile targets for building and deploying each component

---
+++ hack/build-component-image.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -euo pipefail
+
+dir=$1
+fullimagename=$2
+
+tag_prefix="${OS_IMAGE_PREFIX:-"openshift/origin-"}"
+docker_suffix='.centos7'
+
+if [ "${RELEASE_STREAM:-}" = 'prod' ] ; then
+  docker_suffix=''
+fi
+dockerfile="Dockerfile${docker_suffix}"
+
+dfpath=${dir}/${dockerfile}
+
+podman build -f $dfpath -t "$fullimagename" $dir
