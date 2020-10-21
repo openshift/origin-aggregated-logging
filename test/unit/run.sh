1604cbed98a3fb9a769cca2f72cdd8fc5e25c8a9
Jeff Cantrill <jcantril@redhat.com>
2020-10-21 13:27:44 -0400

    fix fluent unit test image

--- test/unit/run.sh
+++ test/unit/run.sh
@@ -2,7 +2,6 @@
 failed="0"
 for d in $(ls $WORKDIR) ; do
     pushd ${WORKDIR}/${d}
-        export GEM_HOME=vendor
         bundle exec rake test
 	    if [ "$?" != "0" ] ; then
               failed="1"
